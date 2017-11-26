277ce04b4b6eda40c473e6e2e072dec7fee59f5a
priyankarchoudhary <priyankar.choudhary@nagarro.com>
2017-11-26 10:00:22 +0530

    first commit

--- my-app/src/app/game-page/game-page.component.ts
+++ my-app/src/app/game-page/game-page.component.ts
@@ -4,6 +4,7 @@ import { CookieService } from 'angular2-cookie/services/cookies.service';
 import {Router} from "@angular/router";
 import {GameServiceService} from "../game-service.service";
 
+
 @Component({
   selector: 'app-game-page',
   templateUrl: './game-page.component.html',
@@ -23,8 +24,8 @@ export class GamePageComponent implements OnInit {
   private catA = 1.5; private catB=1.25;private catC=10;
   private temp: boolean =this.request.showControls;
   private model = JSON.parse(localStorage.getItem("detail"));
-  // private User = this.model.CustomerName;
-  // private Amount =this.model.AccountBalance;
+  private User = this.model.CustomerName;
+  private Amount =this.model.AccountBalance;
   private Email = this.model.EmailId;
   private randomNumber = 12//Math.floor(Math.random() * 36) + 1;
 
@@ -52,26 +53,33 @@ export class GamePageComponent implements OnInit {
         for(var i in this.mainModel){if(this.mainModel[i]>0) {counter = counter + 1;}}
         if(counter==0 || counter > 1){alert("You can select exactly one choice");}
 
-         if(counter==1) {
-             for (var i in this.mainModel) {
-        if(this.mainModel[i]>0)
-        {
-          this.bettingAmount=this.mainModel[i];
-          break;
-        }
-        else {
-          this.loc =this.loc +1;
-        }
-      }
+         if(counter==1)
+         {
+             for (var i in this.mainModel)
+             {
+                if(this.mainModel[i]>0)
+                {
+                   this.bettingAmount=this.mainModel[i];
+                    break;
+                }
+                 else
+                {
+                  this.loc =this.loc +1;
+                }
+             }
+             this.loc+=1; // id of the value on which you bet//alert(this.Email+" "+this.bettingAmount+" "+this.loc+"chekc")
+           if(this.bettingAmount > this.Amount)
+           {
+             (<HTMLInputElement>(document.getElementById("balancecondition"))).click();
+           }
+           else
+           {
+             this.BlockMoneyFunction(this.Email, this.bettingAmount, this.loc);
+           }
 
-            this.loc+=1; // id of the value on which you bet
-
-           alert(this.Email+" "+this.bettingAmount+" "+this.loc+"chekc")
-           this.BlockMoneyFunction(this.Email, this.bettingAmount, this.loc);
-
-    }
-  counter = 0; // make global variable zero
-  }//End of Play Function
+         }
+        counter = 0;
+  }
 
   GameRules() // it is called inside BlockMoneyFunction()
   {
@@ -127,7 +135,25 @@ export class GamePageComponent implements OnInit {
   {
       alert(this.Email+" "+this.bettingAmount+" "+muliplicationFactor);
       (<HTMLInputElement>(document.getElementById("wonbtn"))).click();
-      this.AddMoneyFunction(this.Email, this.bettingAmount, muliplicationFactor)
+      this.AddMoneyFunction(this.Email, this.bettingAmount, muliplicationFactor);
+
+
+    this.request.GetLoginData(this.Email)
+      .subscribe( data => {
+          if(data != null)
+          {
+            if(data.EmailId==this.Email)
+            {
+              this.request.showControls= true;
+              localStorage.setItem("detail",JSON.stringify(data));
+            }
+          }
+          else{
+            console.log("Fail");
+          }
+        }
+      );
+
 
   }
 
